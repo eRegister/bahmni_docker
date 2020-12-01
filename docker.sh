6d151e27c523b248e1aa960e21fea0e7a6acc7da
Makhate Makhate <makhatemakhate@gmail.com>
2020-12-01 15:51:53 +0200

    we just used a new tag for the new image

--- docker.sh
+++ docker.sh
@@ -7,7 +7,7 @@ export https_port=443
 export http_port=86
 export erp_port=8069
 export debug_port=8000
-export image_name=omrsregrepo/bahmni_centosls:69_060818
+export image_name=omrsregrepo/bahmni_docker_092:17112020
 
 if sudo docker ps | grep ${container_name}; then
    sudo docker stop "${container_name}" | sudo xargs docker rm
@@ -32,5 +32,5 @@ if ! sudo docker volume ls -q --filter name="${container_name}"| grep -q "${cont
 else
         echo "Volume ${container_name} exists"
 fi
-docker build --rm -t $image_name --build-arg container_name=${container_name} --build-arg bind_mount_dest=${bind_mount_dest} .
+docker build --rm -t $image_name --no-cache --build-arg container_name=${container_name} --build-arg bind_mount_dest=${bind_mount_dest} .
 docker run -e container_name=${container_name} -e bind_mount_dest=${bind_mount_dest} -it -d -p ${https_port}:443 -p ${http_port}:80 -p ${erp_port}:8069 -p ${debug_port}:8000 --privileged --name $container_name -v /$bind_mount_src:/$bind_mount_dest -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v $container_name:/$container_name $image_name  /usr/sbin/init /bin/bash
