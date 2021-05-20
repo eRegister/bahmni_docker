7230243d393b891028161540cb22c9034e51337f
Makhate Makhate <makhatemakhate@gmail.com>
2021-05-20 14:33:50 +0200

    container name was ommited

--- scripts/odoobackup-daily.sh
+++ scripts/odoobackup-daily.sh
@@ -3,16 +3,11 @@ facility="TestingHiels"
 now="$(date +'%d_%m_%Y_%H_%M_%S')"
 filename="odoo_db_backup_$now".gz
 backupfolder="$HOME/databaseBackups"
+[ ! -d $backupfolder ] && mkdir -p $backupfolder
 fullpathbackupfile="$backupfolder/$filename"
 logfile="$backupfolder/"backup_log_"$(date +'%Y_%m')".txt
 echo "pg_dump started at $(date +'%d-%m-%Y %H:%M:%S')" >> "$logfile"
-pg_dump -U odoo odoo | gzip > "$fullpathbackupfile"
-echo "pg_dump finished at $(date +'%d-%m-%Y %H:%M:%S')" >> "$logfile"
-chown postgres "$fullpathbackupfile"
-chown postgres "$logfile"
-echo "file permission changed" >> "$logfile"
-find "$backupfolder" -name dhis_db_backup_228_* -mtime +10 -exec rm {} \;
-echo "old files deleted" >> "$logfile"
+docker exec -it bahmni_docker_erp-service_1 pg_dump -U odoo odoo | gzip > "$fullpathbackupfile"
 echo "operation finished at $(date +'%d-%m-%Y %H:%M:%S')" >> "$logfile"
 echo "*****************" >> "$logfile"
 exit 0
