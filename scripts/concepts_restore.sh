f5a8922919b4be0d90cd1d95fd5e947f42524523
leqela-prog <leqelatiisetso@gmail.com>
2021-09-01 00:52:32 +0200

    fixing concepts restore scripts

--- scripts/concepts_restore.sh
+++ scripts/concepts_restore.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -i bahmni_docker_emr-service_1 /usr/bin/mysql -u root --password=P@ssw0rd openmrs < /development/eregister_concepts_release/omrs_concept_dictionary.sql 
+docker exec -i bahmni_docker_emr-service_1 /usr/bin/mysql -u root --password=P@ssw0rd openmrs < /development_emr/eregister_concepts_release/omrs_concept_dictionary.sql 
