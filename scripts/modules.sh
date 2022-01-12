67e57d859d35e742fd03bba22ca9db900f112638
leqela-prog <54535149+leqela-prog@users.noreply.github.com>
2022-01-12 19:11:36 +0200

    Update modules.sh
    Correcting the naming of omod

--- scripts/modules.sh
+++ scripts/modules.sh
@@ -5,4 +5,4 @@ docker cp /development_emr/openmrs-module-bahmnicore-release/bahmnicore-omod-0.9
 docker cp /development_emr/openmrs-module-registrationcore-release/registrationcore-1.9.0-SNAPSHOT.omod bahmni_docker_emr-service_1:/opt/openmrs/modules
 docker cp /development_emr/openmrs-module-xdssender-release/xds-sender-1.0.2.omod bahmni_docker_emr-service_1:/opt/openmrs/modules
 docker cp /development_emr/openmrs-module-outgoingmessageexception-release/outgoing-message-exceptions-1.0.0.omod bahmni_docker_emr-service_1:/opt/openmrs/modules
-docker cp /development_emr/openmrs-module-dhisconnector-release/dhisconnector-0.1.5-SNAPSHOT.omod bahmni_docker_emr-service_1:/opt/openmrs/modules
+docker cp /development_emr/openmrs-module-dhisconnector-release/dhisconnector-1.5.5-SNAPSHOT.omod bahmni_docker_emr-service_1:/opt/openmrs/modules
